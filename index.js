fb061ab76e84624f7db7055e14982803e712da3b
Yeahunter <yeahunter@gmail.com>
2018-07-30 13:02:43 +0200

    Az illegalis szinessel irok, timeoutot kapnak. Typo fix
    Signed-off-by: Yeahunter <yeahunter@gmail.com>

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var config = require("./config");
 
 var options = {
     options: {
-        debug: true
+        debug: false
     },
     connection: {
         reconnect: true
     },
     identity: {
-        username: config.username,
+        username: config.botname,
         password: config.password
     },
     channels: config.channels
@@ -19,3 +19,26 @@ var options = {
 var client = new tmi.client(options);
 
 client.connect();
+
+client.on("message", function (channel, userstate, message, self) {
+    // Sajat magam uzeneteit ignoralom
+    if (self) return;
+
+    switch(userstate["message-type"]) {
+        case "action":
+            if(userstate.mod) return;
+
+            // Timeout mindenkire, aki szinessel irkal
+            client.timeout(channel.replace('#', ''), userstate.username, 2, "Nem szabad színessel írkálni!").catch((err) => {
+                console.log('ERR: ' + err);
+            }); 
+            break;
+        case "chat":
+            break;
+        case "whisper":
+            break;
+        default:
+            console.log('Baj van!');
+            break;
+    }
+});
